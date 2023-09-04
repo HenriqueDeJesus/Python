fcd2a6d45712e58a4cfb6f055a6f64c3bb123300
Henrique Guilherme de Jesus Hernandes <136270343+HenriqueDeJesus@users.noreply.github.com>
2023-09-04 15:10:34 -0300

    Create aula50.py

---
+++ aula50.py
@@ -0,0 +1,15 @@
+"""
+Exercício 
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+listas = 0
+
+for nome in lista:
+        listas += 1
+        print(listas, nome, type(nome))
+        
+    
