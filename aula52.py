5fffc2df1416a799660a01d334e32e67856f43b8
Henrique Guilherme de Jesus Hernandes <136270343+HenriqueDeJesus@users.noreply.github.com>
2023-09-08 10:59:19 -0300

    Create aula52.py

---
+++ aula52.py
@@ -0,0 +1,13 @@
+"""
+Tipo tupla - Uma lista imutável
+"""
+nomes = ['Maria', 'Helena', 'Luiz'] # Lista
+# nomes = 'Maria', 'Helena', 'Luiz' # Tupla
+# nomes = ('Maria', 'Helena', 'Luiz') # Tupla mas esecifica
+# Tupla não pode alterar ou acrescentar valor, para fazer isso é necessário converter para lista e depois para tupla de novo
+
+# nomes = tuple(nomes) # Converte a lista para tupla
+# nomes = list(nomes) # Converte uma tupla para lista
+
+print(nomes[-1])
+print(nomes)
