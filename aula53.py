6653c5a66f2f70daee8496e851ee074505db671f
Henrique Guilherme de Jesus Hernandes <136270343+HenriqueDeJesus@users.noreply.github.com>
2023-09-08 10:59:47 -0300

    Create aula53.py

---
+++ aula53.py
@@ -0,0 +1,33 @@
+"""
+enumerate - enumera iteráveis (índices)
+"""
+# [(0, 'Maria'), (1, 'Helena'), (2,'Luiz'), (3, 'João')]
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
+
+for indice, nome in enumerate(lista):
+     print(indice, nome)
+
+# for indice, nome in enumerate(lista):
+#     print(indice, nome, lista[indice])
+
+# for item in enumerate(lista):
+#         indice, nome = item
+#         print(indice, nome)
+
+# for tupla_enumerada in enumerate(lista):
+#     print('FOR da tupla:')
+#     for valor in tupla_enumerada:
+#         print(f'\t{valor}')
+
+# for item in enumerate(lista):
+#     print(item)
+
+# lista_enumerada = list(enumerate(lista))
+
+# lista_enumerada = list(enumerate(lista, start= 19)) # Iniciaria a contagem da lista do 19
+
+
+# print(lista_enumerada)
+# for item in lista_enumerada:
+#     print(item)
