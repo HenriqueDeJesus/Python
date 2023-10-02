29e0b83a187bfdd4d2f006bb1cd59b94dc491c69
Henrique Guilherme de Jesus Hernandes <136270343+HenriqueDeJesus@users.noreply.github.com>
2023-10-02 09:58:51 -0300

    Create aula69.py

---
+++ aula69.py
@@ -0,0 +1,31 @@
+"""
+Escopo de funções em Python
+Escopo significa o local onde aquele código pode atingir.
+Existe o escopo global e local.
+O escopo global é o escopo onde todo o código é alcançavel.
+O escopo local é o escopo onde apenas nomes do mesmo local
+podem ser alcançados.
+"""
+"""É possivel alterar variaveis de escopo externo em escopo interno com a fução global mas lembre-0se que isso é uma má prática"""
+
+
+x = 1
+
+
+def escopo():
+    # global x  # usar a função global é uma má pratica 
+    x = 10
+
+    def outra_funcao():
+        # global x # usar a função global é uma má pratica 
+        x = 11
+        y = 2
+        print(x, y)
+
+    outra_funcao()
+    print(x)
+
+
+print(x)
+escopo()
+print(x)
